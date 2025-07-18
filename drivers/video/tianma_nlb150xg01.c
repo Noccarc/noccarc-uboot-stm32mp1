436a37ceebfa66678ba2309fe0acffeef1f215b8
samarahmad247 <samar.a@noccarc.com>
2025-07-18 17:58:07 +0530

    100 ms delay added

--- drivers/video/tianma_nlb150xg01.c
+++ drivers/video/tianma_nlb150xg01.c
@@ -299,7 +299,7 @@
 		if (ret)
 			return ret;
 
-		mdelay(200); // Wait after bridge setup
+		mdelay(100); // Wait after bridge setup
 
 		dm_gpio_set_value(&priv->backlight_en, true);
 		mdelay(10);
