ce25164cce38141fe457f5b7b99560d0348912f9
samarahmad247 <samar.a@noccarc.com>
2026-03-13 12:47:32 +0530

    normal code .

--- drivers/video/tianma_nlb150xg01.c
+++ drivers/video/tianma_nlb150xg01.c
@@ -284,7 +284,7 @@
 		val = dm_i2c_reg_read(dev1, REG_IRQ_STAT);
 		dm_i2c_reg_write(dev1, REG_IRQ_STAT, val);
 
-		dm_i2c_reg_write(dev1, 0x3c, 0x10);
+		dm_i2c_reg_write(dev1, 0x3c, 0x00);
 		
 		return 0;
 	}
